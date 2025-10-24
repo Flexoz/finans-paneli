229ed14b2ad7e93c1801d9dcf9cde9d1b8787593
Flexoz <102881988+Flexoz@users.noreply.github.com>
2025-10-24 15:15:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 async function fetchData() {
-  const apiUrl = "https://api.frankfurter.app/latest?from=USD&to=TRY,EUR";
-
   try {
-    const response = await fetch(apiUrl);
+    const response = await fetch("https://open.er-api.com/v6/latest/USD");
     const data = await response.json();
 
     const usdTry = data.rates.TRY.toFixed(2);
-    const eurTry = data.rates.EUR.toFixed(2);
+    const eurTry = (data.rates.TRY / data.rates.EUR).toFixed(2);
+    const btcTry = (usdTry * 0.000016).toFixed(2); // sembolik btc değeri
 
     document.getElementById("usd").innerText = `💵 Dolar: ${usdTry} ₺`;
     document.getElementById("eur").innerText = `💶 Euro: ${eurTry} ₺`;
-    document.getElementById("btc").innerText = "₿ Bitcoin: ~1.600.000 ₺ (örnek)";
+    document.getElementById("btc").innerText = `₿ Bitcoin: ${btcTry} ₺`;
   } catch (error) {
     console.error("Veri alınamadı:", error);
     document.querySelector("h1").innerHTML = "Veri alınamadı 😔";
